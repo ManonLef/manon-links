796780d5f7ace8f674390ae6b78329f7c1bd0f03
Manon <manonlefkos@gmail.com>
2023-08-14 18:03:48 +0200

    setup app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { useState } from 'react'
+import Avatar from "./components/Avatar";
+import Intro from "./components/Intro";
+import Socials from "./components/Socials";
 
-function App() {
-   return ()
+export default function App() {
+  return (
+    <div className="bg-white h-screen flex flex-col gap-5 font-Raleway tracking-wider bg-gradient-to-br bg-gradient-to-tr from-violet-500 to-orange-300">
+      <section className="flex flex-col gap-4 items-center text-center text-white bg-white bg-opacity-10 py-5">
+        <Avatar />
+        <Intro />
+      </section>
+      <Socials />
+    </div>
+  )
 }
-
-export default App
