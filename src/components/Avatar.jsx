b22da6bf13dd29404f2541342d201454d31c2cde
Manon <manonlefkos@gmail.com>
2023-08-14 16:36:48 +0200

    add avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+export default function Avatar() {
+  return (
+    <img
+      src="https://avatars.githubusercontent.com/u/81025586?s=400&u=d5f0de302b0337938cf2e833ca526a89982d25f4&v=4"
+      alt="Manon's profile picture"
+      className="rounded-full w-1/6 max-w-xs border-solid border-4 border-white-700"
+    />
+  );
+}
