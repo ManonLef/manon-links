7b95c755a944f82688dc0bc5110dbdfdfbcccf3a
Manon <manonlefkos@gmail.com>
2023-08-14 18:02:49 +0200

    add max width to avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,7 +3,7 @@ export default function Avatar() {
     <img
       src="https://avatars.githubusercontent.com/u/81025586?s=400&u=d5f0de302b0337938cf2e833ca526a89982d25f4&v=4"
       alt="Manon's profile picture"
-      className="rounded-full w-1/6 max-w-xs border-solid border-4 border-white-700"
+      className="rounded-full w-1/6 border-solid border-4 border-white-700 max-w-[200px]"
     />
   );
 }
