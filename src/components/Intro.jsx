352ebde76d7ef7a74c8b659842a244612e776cce
Manon <manonlefkos@gmail.com>
2023-08-14 18:03:03 +0200

    add intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,6 @@
 export default function Intro() {
-  const intro = "Hi, I'm Manon Lef, let's connect!"
+  const intro = "Hi, I'm Manon, let's connect!"
   return (
-    <h1>{intro}</h1>
+    <h1 className="font-semibold text-xl">{intro}</h1>
   )
 }
