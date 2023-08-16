95b018b47242879979718e053e60c32807d70e5f
Manon <manonlefkos@gmail.com>
2023-08-16 15:58:34 +0200

    edit about-section info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 export default function About() {
   const pOne =
-    "Working hard to go from piloting to developing";
+    "Working hard to change careers from aviation into tech";
   
   return (
     <div className="cursor-default flex flex-col gap-2 text-center border-2 border-transparent text-white p-3 mx-auto sm:min-w-[90%] md:min-w-[50%]">
